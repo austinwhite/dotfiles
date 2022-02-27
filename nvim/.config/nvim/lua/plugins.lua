eb505fff91c382ada191a445df45297dc4e2115e
Austin White <austinlldb@gmail.com>
2022-02-27 01:02:08 -0700

    change single quotes to doubles for consistency

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,21 +1,21 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({
-        'git', 
-        'clone', 
-        '--depth', 
-        '1', 
-        'https://github.com/wbthomason/packer.nvim', 
+        "git", 
+        "clone", 
+        "--depth", 
+        "1", 
+        "https://github.com/wbthomason/packer.nvim", 
         install_path
     })
 end
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
     
-    use {'wbthomason/packer.nvim'}
+    use { "wbthomason/packer.nvim" }
 
     use {
         "projekt0n/github-nvim-theme",
@@ -23,6 +23,6 @@ return require('packer').startup(function(use)
     }
 
     if packer_bootstrap then
-      require('packer').sync()
+      require("packer").sync()
     end
 end)
