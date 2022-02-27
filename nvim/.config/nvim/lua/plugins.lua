b8310c930b499561881ce26e313c3a0364fb0503
Austin White <austinlldb@gmail.com>
2022-02-27 05:42:50 -0700

    factor out get_config()

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,10 +2,6 @@ local execute = vim.api.nvim_command
 local fn = vim.fn
 local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 
-function get_config(name)
-    return string.format("require(\"config/%s\")", name)
-end
-
 -- bootstrap packer if not isntalled
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({
@@ -20,13 +16,18 @@ end
 
 return require("packer").startup(function(use)
 
-    use { "wbthomason/packer.nvim" }
+    -- package manager
+    use { "wbthomason/packer.nvim", opt = true }
 
+    -- theme
     use {
         "projekt0n/github-nvim-theme",
-        config = get_config("github-theme")
+        config = function()
+            require("config/github-theme")
+        end,
     }
 
+    -- add symbols to lsp
     use { "onsails/lspkind-nvim" }
 
     -- completion
@@ -37,7 +38,9 @@ return require("packer").startup(function(use)
             { "hrsh7th/cmp-buffer" },
             { "hrsh7th/cmp-path" },
         },
-        config = get_config("completion")
+        config = function()
+            require("config/completion")
+        end,
     }
 
     -- snippets
