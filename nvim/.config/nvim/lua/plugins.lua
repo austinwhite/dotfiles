bcde747db15c0cc29c3375ec03c3f043f05081ba
Austin White <austinlldb@gmail.com>
2022-02-27 01:14:45 -0700

    cmp plugins

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -2,6 +2,7 @@ local execute = vim.api.nvim_command
 local fn = vim.fn
 local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
 
+-- bootstrap packer if not isntalled
 if fn.empty(fn.glob(install_path)) > 0 then
     packer_bootstrap = fn.system({
         "git", 
@@ -15,13 +16,22 @@ end
 
 return require("packer").startup(function(use)
     
+    -- self managed
     use { "wbthomason/packer.nvim" }
 
+    -- theme
     use {
         "projekt0n/github-nvim-theme",
         require('github-theme').setup()
     }
 
+    -- completion plugin
+    use { "hrsh7th/nvim-cmp" } -- completion plugin
+    use { "hrsh7th/cmp-buffer" } -- buffer completions
+    use { "hrsh7th/cmp-path" } -- path completions
+    use { "hrsh7th/cmp-cmdline" } -- cmdline completions
+
+    -- set up full configuration if packer isn't installed
     if packer_bootstrap then
       require("packer").sync()
     end
