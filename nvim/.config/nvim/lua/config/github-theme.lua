14fb88941ba971d80f07a205da1e8aef7d66534e
Austin White <austinlldb@gmail.com>
2022-03-30 02:11:56 -0600

    update

--- nvim/.config/nvim/lua/config/github-theme.lua
+++ nvim/.config/nvim/lua/config/github-theme.lua
@@ -1,19 +1,21 @@
-local status_ok = pcall(require, "github-theme")
+local status_ok, theme = pcall(require, "github-theme")
 if not status_ok then
-    return
+  return
 end
-
-local theme = require("github-theme")
 
 theme.setup({
     theme_style = "dark",
     function_style = "italic",
     sidebars = {"qf", "vista_kind", "terminal", "packer"},
+    dark_float = true,
+    dark_sidebar = true,
   
-    -- Change the "hint" color to the "orange" color, and make the "error" color bright red
-    colors = {hint = "orange", error = "#ff0000"},
+    colors = {
+      hint = "orange",
+      error = "#ff0000",
+      bg_search = "#FFD700",
+    },
   
-    -- Overwrite the highlight groups
     overrides = function(c)
       return {
         htmlTag = {fg = c.red, bg = "#282c34", sp = c.hint, style = "underline"},
