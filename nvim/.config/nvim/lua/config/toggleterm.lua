3e9a7717fc6c91b924fab974501a585fe597eab7
Austin White <austinlldb@gmail.com>
2022-04-02 16:10:48 -0600

    toggleterm

--- nvim/.config/nvim/lua/config/toggleterm.lua
+++ nvim/.config/nvim/lua/config/toggleterm.lua
@@ -42,24 +42,24 @@ local Terminal = require("toggleterm.terminal").Terminal
 local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
 function _LAZYGIT_TOGGLE()
-	lazygit:toggle()
+  lazygit:toggle()
 end
 
 local node = Terminal:new({ cmd = "node", hidden = true })
 
 function _NODE_TOGGLE()
-	node:toggle()
+  node:toggle()
 end
 
 local htop = Terminal:new({ cmd = "htop", hidden = true })
 
 function _HTOP_TOGGLE()
-	htop:toggle()
+  htop:toggle()
 end
 
 local python = Terminal:new({ cmd = "python", hidden = true })
 
 function _PYTHON_TOGGLE()
-	python:toggle()
+  python:toggle()
 end
 
