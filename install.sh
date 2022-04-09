660ea65f836d5deda392dc33c0fbd8f6259c264e
Austin White <austinlldb@gmail.com>
2022-04-09 02:57:18 -0600

    move scripts

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if ! command -v stow $> /dev/null
+then
+    echo "command not found: stow"
+    echo "bootstrapping gnu stow..."
+    sudo apt-get install stow
+fi
+
+if [[ -z "${STOWDIRS}" ]]; then
+    echo "STOWDIRS environment variable is undefined."
+    exit 1
+else
+    dotfile_dirs="${STOWDIRS}"
+fi
+
+for dir in $dotfile_dirs
+do
+    cd ${DOTFILES}
+    echo "stow $dir"
+    stow -D $dir
+    stow $dir
+done
+
