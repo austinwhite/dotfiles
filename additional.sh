ed46f94bd00482c1e43398074d2a93acda059f06
Austin White <austinlldb@gmail.com>
2022-05-14 21:55:51 -0600

    automate crontab

--- additional.sh
+++ additional.sh
@@ -2,3 +2,10 @@
 
 git clone --recurse https://github.com/austinwhite/backup_bookmarks.git ~/.backup_bookmarks
 
+# cronjobs
+crontab -l > mycron
+
+echo "00 */4 * * * cd ~/.backup_bookmarks && ./backup.sh" >> mycron
+
+crontab mycron
+rm mycron
