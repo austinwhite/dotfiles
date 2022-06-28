5fe13471ea1722e1ce397b545af509e1e2ea15f3
Austin White <austinlldb@gmail.com>
2022-06-28 15:27:23 -0600

    switch to nitrogen and lock-screen script

--- scripts/.local/bin/both-monitors.sh
+++ scripts/.local/bin/both-monitors.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+
 xrandr --output eDP1 --mode 1920x1080 --pos 3440x239 --rotate normal --output DP1 --primary --mode 3440x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output DP4 --off --output HDMI1 --off --output VIRTUAL1 --off
+nitrogen --restore &
