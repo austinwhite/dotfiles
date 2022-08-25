7b2d4177e43d274a6506bc59bb7e465dbe5ea25f
Austin White <austinlldb@gmail.com>
2022-08-25 01:40:24 -0600

    nord just doesn't play nice with lualine

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# My Configs
-
-These files are deployed via GNU Stow.
-
-I don't recommend using someone else's config files outright. You shuold use them as reference only.
+# dotfiles
--- nvim/.config/nvim/lua/config/lualine.lua
+++ nvim/.config/nvim/lua/config/lualine.lua
@@ -6,6 +6,7 @@ end
 lualine.setup {
   options = {
     icons_enabled = true,
+    theme = "nord",
     component_separators = { left = '|', right = '|' },
     section_separators = { left = '', right = '' },
     disabled_filetypes = {},
