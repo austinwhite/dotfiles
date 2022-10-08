563c93feecbb8209f2874e2e0e27624d45a79061
Austin White <austinlldb@gmail.com>
2022-10-08 04:28:31 -0600

    update

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,7 @@
-require('globals')	-- global variables
-require('options')	-- non-plugin configuration
-require('plugins')	-- plugin management via Packer
-require('mappings')	-- vim mappings
-require('autocmds')	-- auto commands
+require('impatient')  -- speed up loading times
+
+require('globals')	  -- global variables
+require('options')	  -- non-plugin configuration
+require('plugins')	  -- plugin management via Packer
+require('mappings')	  -- vim mappings
+require('autocmds')	  -- auto commands
