6fcd16e1d524946e1f1e75348aff2dcd891eae46
Austin White <austinlldb@gmail.com>
2023-03-27 16:20:48 -0500

    updates

--- nvim/.config/nvim/lua/core/mapping.lua
+++ nvim/.config/nvim/lua/core/mapping.lua
@@ -9,19 +9,19 @@ keymap("n", "<leader>bp", "<cmd>:bprevious<CR>", {})
 keymap("n", "<leader>bd", "<cmd>:bd<CR>", {})
 
 -- split management
-keymap("n", "<leader>sv", "<C-w>v", opts) -- new vertical
-keymap("n", "<leader>ss", "<C-w>s", opts) -- new horizontal
-keymap("n", "<leader>sq", "<C-w>q", opts) -- quit current
+keymap("n", "<leader>sv", "<C-w>v", {}) -- new vertical
+keymap("n", "<leader>ss", "<C-w>s", {}) -- new horizontal
+keymap("n", "<leader>sq", "<C-w>q", {}) -- quit current
 
-keymap("n", "<C-h>", "<C-w>h", opts) -- move left
-keymap("n", "<C-j>", "<C-w>j", opts) -- move down
-keymap("n", "<C-k>", "<C-w>k", opts) -- move up
-keymap("n", "<C-l>", "<C-w>l", opts) -- move right
+keymap("n", "<C-h>", "<C-w>h", {}) -- move left
+keymap("n", "<C-j>", "<C-w>j", {}) -- move down
+keymap("n", "<C-k>", "<C-w>k", {}) -- move up
+keymap("n", "<C-l>", "<C-w>l", {}) -- move right
 
-keymap("n", "<Left>", ":vertical resize +3<CR>", opts) -- vertical resize
-keymap("n", "<Right>", ":vertical resize -3<CR>", opts) -- vertical resize
-keymap("n", "<Up>", ":resize +3<CR>", opts) -- horizontal resize
-keymap("n", "<Down>", ":resize -3<CR>", opts) -- horizontal resize
+keymap("n", "<Left>", ":vertical resize +3<CR>", {}) -- vertical resize
+keymap("n", "<Right>", ":vertical resize -3<CR>", {}) -- vertical resize
+keymap("n", "<Up>", ":resize +3<CR>", {}) -- horizontal resize
+keymap("n", "<Down>", ":resize -3<CR>", {}) -- horizontal resize
 
-keymap("n", "<leader>sh", "<C-w>t<C-w>H", opts) -- swap horizontal --> vertical
-keymap("n", "<leader>sk", "<C-w>t<C-w>K", opts) -- swap vertical --> horizontal
+keymap("n", "<leader>sh", "<C-w>t<C-w>H", {}) -- swap horizontal --> vertical
+keymap("n", "<leader>sk", "<C-w>t<C-w>K", {}) -- swap vertical --> horizontal
--- nvim_v1/.config/nvim/init.lua
+++
@@ -1,7 +0,0 @@
-require('impatient')  -- speed up loading times
-
-require('globals')	  -- global variables
-require('options')	  -- non-plugin configuration
-require('plugins')	  -- plugin management via Packer
-require('mappings')	  -- vim mappings
-require('autocmds')	  -- auto commands
