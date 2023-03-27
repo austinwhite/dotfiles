a4b9d64f6a584e516dfd9eab62df6b7931c34c8a
Austin White <austinlldb@gmail.com>
2023-03-27 14:15:57 -0500

    new nvim config

--- nvim/.config/nvim/after/plugin/alpha.lua
+++ nvim/.config/nvim/after/plugin/alpha.lua
@@ -1,44 +1,40 @@
-local alpha = require('alpha')
-local dashboard = require('alpha.themes.dashboard')
+local alpha = require("alpha")
+local dashboard = require("alpha.themes.dashboard")
 
 local function footer()
-  local total_plugins = #vim.tbl_keys(packer_plugins)
-  local datetime = os.date(" %m-%d-%Y   %H:%M:%S")
-  local version = vim.version()
-  local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
+	local total_plugins = #vim.tbl_keys(packer_plugins)
+	local datetime = os.date(" %m-%d-%Y   %H:%M:%S")
+	local version = vim.version()
+	local nvim_version_info = "   v" .. version.major .. "." .. version.minor .. "." .. version.patch
 
-  return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
+	return datetime .. "   " .. total_plugins .. " plugins" .. nvim_version_info
 end
 
 dashboard.section.header.val = {
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    "",
-    ' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
-    ' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
-    ' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
-    ' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
-    ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
-    ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
-    '',
-    ' [ TIP: To exit Neovim, just power off your computer. ] ',
-    '',
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	"",
+	" ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+	" ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+	" ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+	" ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+	" ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+	" ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+	"",
+	" [ TIP: To exit Neovim, just power off your computer. ] ",
+	"",
 }
 
-dashboard.section.header.opts.hl = 'Constant'
-dashboard.section.buttons.val = {nil}
+dashboard.section.header.opts.hl = "Constant"
+dashboard.section.buttons.val = { nil }
 dashboard.section.footer.val = footer()
-dashboard.section.footer.opts.hl = 'Function'
+dashboard.section.footer.opts.hl = "Function"
 
 alpha.setup(dashboard.opts)
-
--- vim.cmd [[
---   autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
--- ]]
--- nvim/.config/nvim/lua/core/mapping.lua
+++ nvim/.config/nvim/lua/core/mapping.lua
@@ -1,6 +1,27 @@
-vim.g.mapleader = " "
+local keymap = vim.keymap.set
+local g = vim.g
+
+g.mapleader = " "
 
 -- buffers
-vim.keymap.set("n", "<leader>bn", "<cmd>:bnext<CR>", {})
-vim.keymap.set("n", "<leader>bp", "<cmd>:bprevious<CR>", {})
-vim.keymap.set("n", "<leader>bd", "<cmd>:bd<CR>", {})
+keymap("n", "<leader>bn", "<cmd>:bnext<CR>", {})
+keymap("n", "<leader>bp", "<cmd>:bprevious<CR>", {})
+keymap("n", "<leader>bd", "<cmd>:bd<CR>", {})
+
+-- split management
+keymap("n", "<leader>sv", "<C-w>v", opts) -- new vertical
+keymap("n", "<leader>ss", "<C-w>s", opts) -- new horizontal
+keymap("n", "<leader>sq", "<C-w>q", opts) -- quit current
+
+keymap("n", "<C-h>", "<C-w>h", opts) -- move left
+keymap("n", "<C-j>", "<C-w>j", opts) -- move down
+keymap("n", "<C-k>", "<C-w>k", opts) -- move up
+keymap("n", "<C-l>", "<C-w>l", opts) -- move right
+
+keymap("n", "<Left>", ":vertical resize +3<CR>", opts) -- vertical resize
+keymap("n", "<Right>", ":vertical resize -3<CR>", opts) -- vertical resize
+keymap("n", "<Up>", ":resize +3<CR>", opts) -- horizontal resize
+keymap("n", "<Down>", ":resize -3<CR>", opts) -- horizontal resize
+
+keymap("n", "<leader>sh", "<C-w>t<C-w>H", opts) -- swap horizontal --> vertical
+keymap("n", "<leader>sk", "<C-w>t<C-w>K", opts) -- swap vertical --> horizontal
