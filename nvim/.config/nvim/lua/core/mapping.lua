743dcca278257ceba8d2d774e81ec01809af76d2
Austin White <austinlldb@gmail.com>
2023-03-29 19:53:02 -0500

    updates

--- nvim/.config/nvim/lua/core/mapping.lua
+++ nvim/.config/nvim/lua/core/mapping.lua
@@ -8,20 +8,11 @@ keymap("n", "<TAB>", "<cmd>:bnext<CR>", {})
 keymap("n", "<S-TAB>", "<cmd>:bprevious<CR>", {})
 keymap("n", "<leader>bd", "<cmd>:bd<CR>", {})
 
--- split management
-keymap("n", "<leader>sv", "<C-w>v", {}) -- new vertical
-keymap("n", "<leader>ss", "<C-w>s", {}) -- new horizontal
-keymap("n", "<leader>sq", "<C-w>q", {}) -- quit current
-
-keymap("n", "<C-h>", "<C-w>h", {}) -- move left
-keymap("n", "<C-j>", "<C-w>j", {}) -- move down
-keymap("n", "<C-k>", "<C-w>k", {}) -- move up
-keymap("n", "<C-l>", "<C-w>l", {}) -- move right
-
+-- splits 
 keymap("n", "<Left>", ":vertical resize +3<CR>", {}) -- vertical resize
 keymap("n", "<Right>", ":vertical resize -3<CR>", {}) -- vertical resize
 keymap("n", "<Up>", ":resize +3<CR>", {}) -- horizontal resize
 keymap("n", "<Down>", ":resize -3<CR>", {}) -- horizontal resize
 
-keymap("n", "<leader>sh", "<C-w>t<C-w>H", {}) -- swap horizontal --> vertical
-keymap("n", "<leader>sk", "<C-w>t<C-w>K", {}) -- swap vertical --> horizontal
+keymap("n", "<leader>sh", "<C-w>t<C-w>H", {}) -- swap horizontal -> vertical
+keymap("n", "<leader>sk", "<C-w>t<C-w>K", {}) -- swap vertical -> horizontal
