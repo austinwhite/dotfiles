f7c7f202a831e58b94f4189693acb9d825f828a8
Austin White <austinlldb@gmail.com>
2023-07-09 16:18:55 -0500

    remove submodules

---
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,79 @@
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
+
+lsp.ensure_installed({})
+
+-- fix undefined 'vim'
+lsp.configure("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+		},
+	},
+})
+
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+	["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	["<C-Space>"] = cmp.mapping.complete(),
+})
+
+cmp_mappings["<Tab>"] = nil
+cmp_mappings["<S-Tab>"] = nil
+
+lsp.setup_nvim_cmp({
+	mapping = cmp_mappings,
+})
+
+lsp.set_sign_icons({
+	error = "✘",
+	warn = "▲",
+	hint = "⚑",
+	info = "»",
+})
+
+require("luasnip.loaders.from_vscode").lazy_load()
+
+local mason = require("mason")
+mason.setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+		border = "rounded",
+	},
+})
+
+-- add a border to lspconfig window
+local lspwin = require("lspconfig.ui.windows")
+lspwin.default_options.border = "rounded"
+
+lsp.setup()
+
+local null_ls = require("null-ls")
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+
+null_ls.setup({
+	debug = false,
+	sources = {
+		formatting.prettier,
+		formatting.stylua,
+		formatting.clang_format,
+		formatting.cmake_format,
+		formatting.autoflake,
+	},
+})
+
+vim.keymap.set("n", "<leader>I", function()
+	vim.lsp.buf.format()
+end, {})
