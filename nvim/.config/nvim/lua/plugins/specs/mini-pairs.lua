8f9e28bc7199e5a230452dc486cdb03b8a311632
austin <austinlld@gmail.com>
2024-11-15 19:13:52 -0600

    autopairs to mini.pairs

---
+++ nvim/.config/nvim/lua/plugins/specs/mini-pairs.lua
@@ -0,0 +1,8 @@
+return {
+  "echasnovski/mini.pairs",
+  event = { "InsertEnter", "CmdlineEnter" },
+  version = false,
+  config = function()
+    require("mini.pairs").setup({})
+  end,
+}
--- nvim/.config/nvim/utils.lua
+++ nvim/.config/nvim/utils.lua
@@ -1,15 +1,15 @@
 local M = {}
 
-local MiniFiles = require("mini.files")
-local Oil = require("oil")
-
 M.minifiles_toggle = function(...)
+  local MiniFiles = require("mini.files")
+
   if not MiniFiles.close() then
     MiniFiles.open(...)
   end
 end
 
 M.oil_toggle = function(...)
+  local Oil = require("oil")
   local curr_buff_ft = vim.bo.filetype
 
   if curr_buff_ft == "oil" then
