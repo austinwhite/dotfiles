dad28560363d63ca08d98174a03316d2ba73822b
Austin White <austinlldb@gmail.com>
2025-04-28 16:38:30 -0500

    remove some bloat

--- nvim/.config/nvim/lua/core/utils.lua
+++ nvim/.config/nvim/lua/core/utils.lua
@@ -8,15 +8,4 @@ M.minifiles_toggle = function(...)
   end
 end
 
-M.oil_toggle = function(...)
-  local Oil = require("oil")
-  local curr_buff_ft = vim.bo.filetype
-
-  if curr_buff_ft == "oil" then
-    Oil.close()
-  else
-    Oil.open(...)
-  end
-end
-
 return M
