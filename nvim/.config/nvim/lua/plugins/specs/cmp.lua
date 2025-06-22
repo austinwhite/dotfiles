0a543bc74384a366ce8d5aff02741dcbc6b5c264
Austin White <austinlldb@gmail.com>
2025-06-21 23:20:53 -0500

    updates

--- nvim/.config/nvim/lua/plugins/specs/cmp.lua
+++ nvim/.config/nvim/lua/plugins/specs/cmp.lua
@@ -1,88 +1,20 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    {
-      "L3MON4D3/LuaSnip",
-      build = (function()
-        if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-          return
-        end
-        return "make install_jsregexp"
-      end)(),
-      dependencies = {
-        {
-          "rafamadriz/friendly-snippets",
-          config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-          end,
-        },
-      },
+  "saghen/blink.cmp",
+  dependencies = { "rafamadriz/friendly-snippets" },
+
+  version = "1.*",
+
+  opts = {
+    keymap = { preset = "default" },
+
+    appearance = {
+      nerd_font_variant = "mono",
     },
-    "saadparwaiz1/cmp_luasnip",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
+
+    completion = { documentation = { auto_show = false } },
+    signature = { enabled = true },
+
+    fuzzy = { implementation = "prefer_rust_with_warning" },
   },
-  config = function()
-    local cmp = require("cmp")
-    local luasnip = require("luasnip")
-    luasnip.config.setup({})
-
-    local border_opts = {
-      border = {
-        { "┌", "CmpBorder" },
-        { "─", "CmpBorder" },
-        { "┐", "CmpBorder" },
-        { "│", "CmpBorder" },
-        { "┘", "CmpBorder" },
-        { "─", "CmpBorder" },
-        { "└", "CmpBorder" },
-        { "│", "CmpBorder" },
-      },
-      winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
-    }
-    cmp.setup({
-      window = {
-        completion = cmp.config.window.bordered(border_opts),
-        documentation = cmp.config.window.bordered(border_opts),
-      },
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
-      completion = { completeopt = "menu,menuone,noinsert" },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<Enter>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete({}),
-        ["<C-l>"] = cmp.mapping(function()
-          if luasnip.expand_or_locally_jumpable() then
-            luasnip.expand_or_jump()
-          end
-        end, { "i", "s" }),
-        ["<C-h>"] = cmp.mapping(function()
-          if luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          end
-        end, { "i", "s" }),
-      }),
-      sources = {
-        {
-          name = "lazydev",
-          group_index = 0,
-        },
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "path" },
-      },
-    })
-
-    local fg_color = vim.api.nvim_get_hl(0, { name = "LineNr" }).fg
-    vim.api.nvim_set_hl(0, "CmpBorder", { fg = fg_color })
-  end,
+  opts_extend = { "sources.default" },
 }
