e1c43534ab7016fc89e84366c43c961027aff4e5
austin <austinlld@gmail.com>
2025-06-22 00:16:42 -0500

    updates

--- nvim/.config/nvim/lua/plugins/specs/lsp.lua
+++ nvim/.config/nvim/lua/plugins/specs/lsp.lua
@@ -20,12 +20,25 @@ return {
 
     require("mason-lspconfig").setup({
       automatic_enable = true,
-      ensure_installed = { },
+      ensure_installed = {},
       handlers = {
         function(server_name)
           require("lspconfig")[server_name].setup({ capabilities = capabilities })
         end,
       },
     })
+
+    vim.diagnostic.config({
+      underline = false,
+      virtual_text = true,
+    })
+
+    if vim.g.have_nerd_font then
+      local signs = { Error = " ", Warn = " ", Hint = "󰌶 ", Info = " " }
+      for type, icon in pairs(signs) do
+        local hl = "DiagnosticSign" .. type
+        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      end
+    end
   end,
 }
